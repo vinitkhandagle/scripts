96e96b919133c0604dc6408ddcde6cc3dfccd977
vinitkhandagle <vinit.khandagle@gmail.com>
2013-04-25 01:05:36 +0530

    Adding Fab File

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,37 @@
 from fabric.api import *
+import os
+import shutil
+import stat
 
-@hosts('localhost', 'redhatbox')
-def mytask():
+env.hosts.extend(['x.p.w1', 'x.p.w2'])
+
+def type_host():
 	run('hostname')
 
-@hosts('redhatbox')
-def pkg_install(name):
-	sudo('yum -y install %s' % name)
+def type_apache():
+	sudo('/etc/init.d/httpd status')
 
 
-@hosts('vinit@redhatbox')
-def hg_clone():
-	clone_dir = '/home/vinit/clone_dir'
-	with cd(clone_dir):
-		run("hg clone https://hg.dev.xcite.com/trunk/pci")
+@hosts()
+def backup_site():
+	sudo('cp -ar /home/vinit/newdir /home/vinit/new_fabric_site_`date +%F`')
+	
 
 
+def deploy():
+	if os.path.exists('code_dirs'):
+		local('rm -rf code_dirs')
+	local('mkdir code_dirs')
+	local('cp -ar /var/www/site/app code_dirs')
+	local('cp -ar /var/www/site/skin code_dirs')
+	local('cp -ar /var/www/site/js code_dirs')
+	
+	if os.path.exists('code_dirs.tar.gz'):
+		local('rm -rf code_dirs.tar.gz')
+	local('tar -czf code_dirs.tar.gz code_dirs > /dev/null')
+	put('code_dirs.tar.gz', '/home/vinit/code_dirs.tar.gz')
+	run('tar -zxf code_dirs.tar.gz')
+	run('rsync -avzr code_dirs/* code_sync')
+    
+	sudo('chown -R vinit:nobody code_sync')
+	backup_site()
